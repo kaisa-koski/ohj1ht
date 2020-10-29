0e377394621810ba16c01337c24e709481a7b41d
Koski Kaisa Maria <kaisa.m.koski@gmail.com>
2020-10-29 11:48:31 +0200

    Paljon muutoksia koodiin, valmiimpi versio pelistä

--- Kirjoituspeli/Kirjoituspeli/Kirjoituspeli.cs
+++ Kirjoituspeli/Kirjoituspeli/Kirjoituspeli.cs
@@ -1,50 +1,92 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.IO;
+using System.Linq;
 using Jypeli;
 using Jypeli.Assets;
 using Jypeli.Controls;
 using Jypeli.Widgets;
 
-public class Kirjoituspeli : Game //TODO: Pitäisi löytyä taulukko ja silmukka. Taulukko on jo mallilauseiden varastoinnissa. Silmukka varmaankin parhaiden aikojen vertailussa?
+public class Kirjoituspeli : Game
 {
-    string[] lauseet;   //TODO: Voidaanko attribuutit välttää ja onko se tavoiteltavaa?
-    Label mallilause;
+    private List<string> lauseet;   //TODO: Ohjaaja: Onko jotain sääntöä milloin attribuutteja pitää/kannattaa käyttää?
+    private Label mallilause;
+    private Timer aikalaskuri;
+    private InputWindow kysymysikkuna;
+    private int toistot = 3;
+    
 
-    //TODO: Täydennä dokumentointi
     /// <summary>
     /// Aliohjelma käynnistää pelin 
     /// </summary>
     public override void Begin()
     {
-        Camera.ZoomToLevel();
+        //Camera.ZoomToLevel(); //Ei taida tässä vaikuttaa mitenkään?
         Level.BackgroundColor = Color.SkyBlue;
         LataaLauseet();
         MallilauseEsille();
-        LuoKysymysikkuna();
+        LuoAikaLaskuri();
 
-        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli"); //TODO: Tätä ei ehkä tarvitse, jos peliin tulee alkuvalikko myöhemmin
+        Keyboard.Listen(Key.LeftShift, ButtonState.Pressed, VastaaKysymykseen, null);
+    }
+
+    /// <summary>
+    /// Avaa kirjoitusikkunan ja laittaa sekuntikellon päälle.
+    /// </summary>
+    private void VastaaKysymykseen()
+    {
+        if (!(toistot < 0))             //TODO: Ohjaaja: Olisiko tähän parempaa vaihtoehtoa, ettei shift enää lopussa toteuttaisi tätä aliohjelmaa?
+        {
+            LuoKysymysikkuna();
+            aikalaskuri.Start();
+        }
     }
 
 
     /// <summary>
-    /// Aliohjelma arpoo mallilausetaulukosta satunnaisen 
-    /// mallilauseen ja lisää sen ruudulle.
+    /// Aliohjelma lisää kirjoitettavan mallilauseen ruudulle.
     /// </summary>
     private void MallilauseEsille()
     {
-        mallilause = new Label(lauseet[RandomGen.NextInt(lauseet.Length)]);
+        mallilause = new Label(ArvoLause());
         mallilause.Position = new Vector(0, Level.Top * 0.5);
+        Label shiftOhje = new Label("(Paina Shift aloittaaksesi kirjoittaminen)");
+        shiftOhje.TextColor = Color.White;
         Add(mallilause);
+        Add(shiftOhje);
     }
 
 
     /// <summary>
-    /// Lataa kirjoitettavat esimerkkilauseet tekstitiedostosta.
+    /// Arpoo mallilauseista satunnaisen lauseen, kunnes lauseita on arvottu attribuuttine 
+    /// määritellyn toistojen verran. Kun lause on arvottu, se poistetaan listasta, joten 
+    /// samaa lausetta ei tule samalla pelikerralla kahdesti.
+    /// </summary>
+    /// <returns>Mallilause tai ilmoitus "Valmis!"</returns>
+    private string ArvoLause()
+    {
+        toistot--;
+        if (toistot < 0)
+        {
+            return "Valmis!";
+            //TODO: Tässä voisi tehdä jotain sille, ettei aikalaskuri ja kysymysikkuna mene enää lopussa päälle.
+            //TODO: Ajan vertailu top5 listan kanssa 
+        }
+        int lauseenIndeksi = RandomGen.NextInt(lauseet.Count());
+        string lause = lauseet[lauseenIndeksi];
+        lauseet.RemoveAt(lauseenIndeksi);
+        return lause;
+    }
+
+
+    /// <summary>
+    /// Lataa kirjoitettavat esimerkkilauseet tekstitiedostosta ja lisää ne listaan.
     /// </summary>
     private void LataaLauseet()
     {
-        lauseet = File.ReadAllLines("lauseet.txt");
+        lauseet = File.ReadAllLines("lauseet.txt").ToList();
     }
 
 
@@ -52,10 +94,10 @@ public class Kirjoituspeli : Game //TODO: Pitäisi löytyä taulukko ja silmukka
     /// Aliohjelma luo ja lisää peliin kysymysikkunan, johon käyttäjä 
     /// kirjoittaa vastauksensa.
     /// </summary>
-    /// <param name="viesti">Teksti, joka tulee kirjoitusikkunan yläpuolelle</param>
-    private void LuoKysymysikkuna(string viesti = "Kirjoita tähän")
+    /// <param name="viesti">Teksti, joka tulee kirjoitusikkunan yläosaan, oletuksena tyhjä</param>
+    private void LuoKysymysikkuna(string viesti = "")
     {
-        InputWindow kysymysikkuna = new InputWindow(viesti);
+        kysymysikkuna = new InputWindow(viesti);
         kysymysikkuna.TextEntered += ProcessInput;
         kysymysikkuna.InputBox.Color = Color.White;
         kysymysikkuna.BorderColor = Color.Darker(Color.SkyBlue, 50);
@@ -68,13 +110,37 @@ public class Kirjoituspeli : Game //TODO: Pitäisi löytyä taulukko ja silmukka
     /// kirjoittamaa tekstiä mallilauseeseen.
     /// </summary>
     /// <param name="ikkuna">Teksti-ikkuna, johon käyttäjä kirjoittaa vastauksensa</param>
-    void ProcessInput(InputWindow ikkuna)
+    private void ProcessInput(InputWindow ikkuna)
     {
         string vastaus = ikkuna.InputBox.Text;
-        if (vastaus != mallilause.Text)  MessageDisplay.Add("Oikein!"); //TODO: Pysähtyykö kello vasta silloin kun lause menee oikein?
-        else LuoKysymysikkuna("Yritä uudelleen!");
+        if (vastaus == mallilause.Text)
+        {
+            aikalaskuri.Pause();
+            mallilause.Text = "Oikein!";
+            Timer.SingleShot(3.0,
+            delegate { mallilause.Text = ArvoLause(); }
+        );
+        }
+        else
+        {
+            LuoKysymysikkuna("Yritä uudelleen!"); //TODO: Laittaisiko tähän kellon menemään pauselle ja näyttäisi mikä meni väärin?
+        }
     }
 
 
+    /// <summary>
+    /// Aliohjelma luo ja lisää peliin aikalaskurin.
+    /// </summary>
+    private void LuoAikaLaskuri()
+    {
+        aikalaskuri = new Timer();
+
+        Label aikaNaytto = new Label();
+        aikaNaytto.TextColor = Color.White;
+        aikaNaytto.DecimalPlaces = 2;
+        aikaNaytto.BindTo(aikalaskuri.SecondCounter);
+        aikaNaytto.Position = new Vector(Level.Right * 0.9, Level.Top * 0.9); //TODO: Onko parempaa tapaa näiden paikkojen asettamiseen?
+        Add(aikaNaytto);
+    }
 }
 
